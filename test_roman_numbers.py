ed1a79552877e2210c55a3e385e0bf0104f262b5
martin98923 <martinrodriguito@gmail.com>
2019-08-14 12:39:34 -0300

    comit local

--- test_roman_numbers.py
+++ test_roman_numbers.py
@@ -5,7 +5,7 @@ from roman_numbers import roman_to_decimal
 class TestRomanNumbers(unittest.TestCase):
     def test_roman_I_to_decimal(self):
         decimal_number = roman_to_decimal('I')
-        self.asserEqual(decimal_number, 1)
+        self.assertEqual(decimal_number, 1)
 
 
 if __name__ == '__main__':
